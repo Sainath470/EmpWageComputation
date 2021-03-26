710adabf1bf5b93a624683c731b2c34697f405ec
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-26 18:58:05 +0530

    Added: Attendance Check

--- OopsEmpWage.java
+++ OopsEmpWage.java
@@ -1,2 +1,33 @@
-"Welcome to Eployee Wage Computation"
+//"Welcome to Eployee Wage Computation"
+import java.util.*;
 
+public class OopsEmpWage
+{
+
+		public static int attendance()
+		{
+				Random emp = new Random();
+				int attend=(int)(Math.floor(Math.random()*10)%3);
+				return attend;
+		}
+
+
+		public static void EmpCheck()
+		{
+			int check=attendance();
+			if(check==0)
+			{
+				System.out.println("Employee is Present");
+			}
+			else
+			{
+				System.out.println("EMployee is Absent");
+			}
+		}
+
+
+		public static void main(String args[])
+		{
+			EmpCheck();
+		}
+}
