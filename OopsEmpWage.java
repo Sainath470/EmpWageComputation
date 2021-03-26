c838f954cdbbdfd081bf7f08ea890868e347c89c
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-26 20:28:57 +0530

    Added: Wage per hour and Full day hour

--- OopsEmpWage.java
+++ OopsEmpWage.java
@@ -3,31 +3,41 @@ import java.util.*;
 
 public class OopsEmpWage
 {
+		static int TotalWage=0;
+		static int WagePerHr=20;
+		static int Full_time=8;
 
-		public static int attendance()
-		{
+			public static int attendance()
+			{
 				Random emp = new Random();
 				int attend=(int)(Math.floor(Math.random()*10)%3);
 				return attend;
-		}
-
-
-		public static void EmpCheck()
-		{
-			int check=attendance();
-			if(check==0)
-			{
-				System.out.println("Employee is Present");
 			}
-			else
+
+
+			public void EmpCheck()
 			{
-				System.out.println("EMployee is Absent");
+				int check=attendance();
+
+				if(check==0)
+				{
+					System.out.println("Employee is Present");
+					TotalWage+=Full_time*WagePerHr;
+					System.out.println("Total Wage is "+TotalWage);
+
+				}
+				else
+				{
+					System.out.println("Employee is Absent");
+				}
 			}
-		}
 
 
-		public static void main(String args[])
-		{
-			EmpCheck();
-		}
+			public static void main(String args[])
+			{
+
+				OopsEmpWage emp1=new OopsEmpWage();
+				emp1.EmpCheck();
+
+			}
 }
